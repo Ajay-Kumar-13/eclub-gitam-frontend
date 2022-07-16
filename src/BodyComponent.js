068f168d87388814f0c90b8c6ca68c736ce0bb8b
Ajaykumar-13 <ajaykumar13.p@gmail.com>
2022-07-16 08:38:47 +0530

    upload all files

---
+++ src/BodyComponent.js
@@ -0,0 +1,56 @@
+import { Button, Modal, Form, ModalBody } from "react-bootstrap";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function BodyComponent() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const navigate = useNavigate();
+    return (
+        <div className="about">
+            <img
+                src="	https://s4.aconvert.com/convert/p3r68-cdx67/adqym-csv3x.svg"
+                width="500"
+                height="500"
+                className="d-inline-block align-top"
+                alt="e-club logo"
+            />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <Button variant="danger" onClick={handleShow} className="login">Login with Gitam</Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>User Details</Modal.Title>
+                </Modal.Header>
+                <ModalBody>
+                    <Form>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Registration Number</Form.Label>
+                            <Form.Control type="email" placeholder="Enter Registration Number" />
+                            <Form.Text className="text-muted">
+                                We'll never share your Details with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Select Year</Form.Label>
+                            <Form.Select>
+                                <option>First</option>
+                                <option>Second</option>
+                                <option>Third</option>
+                                <option>Fourth</option>
+                            </Form.Select>
+                        </Form.Group>
+                    </Form>
+                </ModalBody>
+                <Modal.Footer>
+                    <Button variant="outline-secondary" onClick={handleClose}>Close</Button>
+                    <Button variant="outline-primary" onClick={() => navigate('home')}>Save</Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    )
+}
+
+export default BodyComponent;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import HomeComponent from './HomeComponent';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/home" element={<HomeComponent />} />
+			</Routes>
+		</BrowserRouter>
+
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
