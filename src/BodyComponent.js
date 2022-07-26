6f6601477623fcaa4d597f832b52198aa559423d
Ajaykumar-13 <ajaykumar13.p@gmail.com>
2022-07-26 09:15:14 +0530

    complete user registration in database

--- src/BodyComponent.js
+++ src/BodyComponent.js
@@ -1,18 +1,12 @@
-import { Button, Modal, Form, ModalBody } from "react-bootstrap";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { Button } from "react-bootstrap";
+import React from "react";
 
 function BodyComponent() {
 
     function googleAuth() {
-        window.open("http://localhost:3001/auth/google","_self");
+        window.open("http://localhost:3001/auth/google", "_self");
     }
 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const navigate = useNavigate();
     return (
         <div className="about">
             <img
@@ -25,36 +19,6 @@ function BodyComponent() {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <Button variant="danger" className="login" onClick={googleAuth}>Login with Gitam</Button>
-
-            <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
-                <Modal.Header closeButton>
-                    <Modal.Title>User Details</Modal.Title>
-                </Modal.Header>
-                <ModalBody>
-                    <Form>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Registration Number</Form.Label>
-                            <Form.Control type="email" placeholder="Enter Registration Number" />
-                            <Form.Text className="text-muted">
-                                We'll never share your Details with anyone else.
-                            </Form.Text>
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Select Year</Form.Label>
-                            <Form.Select>
-                                <option>First</option>
-                                <option>Second</option>
-                                <option>Third</option>
-                                <option>Fourth</option>
-                            </Form.Select>
-                        </Form.Group>
-                    </Form>
-                </ModalBody>
-                <Modal.Footer>
-                    <Button variant="outline-secondary" onClick={handleClose}>Close</Button>
-                    <Button variant="outline-primary" onClick={() => navigate('home')}>Save</Button>
-                </Modal.Footer>
-            </Modal>
         </div>
     )
 }
--- src/HomeComponent.js
+++ src/HomeComponent.js
@@ -5,28 +5,38 @@ import Footer from './Footer';
 import CarouselComponent from './CarouselComponent';
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import UserdetailsModel from './UserdetailsModel';
 
 function HomeComponent() {
 
+    const [Registered, setRegistered] = useState(false);
     const [user, setUser] = useState("");
 
     useEffect(() => {
         axios.get("/auth/login/success")
-            .then(resObject => setUser(resObject.data.user))
+            .then(resObject => {
+                axios.get("/auth/newUser/" + resObject.data.user._json.email)
+                    .then(res => {
+                        setUser(resObject.data.user._json.email);
+                        setRegistered(res.data.registered);
+                    });
+            })
             .catch(err => console.log(err))
-
     }, [])
 
-    console.log(user);
 
     return (
         <React.Fragment>
-            <Navbarmain />
-            <Jumbotron />
-            <CarouselComponent />
-            <Footer />
+            {Registered ?<Navbarmain /> : null}
+            {Registered ? null : <UserdetailsModel email={user}/>}
+            {Registered ? <Jumbotron /> : null}
+            {Registered ? <CarouselComponent /> : null}
+            {Registered ? <Footer /> : null}
         </React.Fragment>
     );
+
+
+
 }
 
 export default HomeComponent;
